81f620ef54412553e12cc8c4c327858414aeb3ba
tobijust <just.tobi@web.de>
2016-02-15 11:33:33 +0100

    Initial Commit

---
+++ LampColorUDP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LampColorUDP
+//
+//  Created by Tobias Just on 15.02.16.
+//  Copyright © 2016 Tobias Just. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
